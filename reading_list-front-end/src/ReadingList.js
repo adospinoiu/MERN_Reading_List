584070f072fc10282bc7a91d59d4602c94e9ede1
adospinoiu <alex.dospinoiu@gmail.com>
2021-04-23 05:57:41 -0700

    Added the three input fields for adding a new book

--- reading_list-front-end/src/ReadingList.js
+++ reading_list-front-end/src/ReadingList.js
@@ -6,13 +6,34 @@ import MenuBookIcon from '@material-ui/icons/MenuBook';
 function ReadingList() {
     return (
         <div className="readinglist">
-            <div className="readinglist__header">
-                <MenuBookIcon />
-                <p>Add New Book to Reading List</p>
-            </div>
-
             <div className="readinglist__addnewbook">
+                <div className="addnewbook__header">
+                    <MenuBookIcon />
 
+                    <p>Add New Book to Reading List</p>
+                </div>
+            
+                <div className="addnewbook__inputFields">
+                    <form>
+                        <input
+                            // value={input}
+                            placeholder="Title"
+                            type="text"
+                        />
+                        <input 
+                            // value={input}
+                            placeholder="Author"
+                            type="text"
+                        />
+                        <input
+                            // value={input}
+                            placeholder="Recommended by"
+                            type="text"
+                        />
+
+                        <button type="submit">Add to List</button>
+                    </form>
+                </div>
             </div>
 
             <div className="readinglist__currentlyreading">
@@ -22,7 +43,7 @@ function ReadingList() {
             <div className="readinglist__futurereading">
 
             </div>
-        </div>
+        </div >
     )
 }
 
