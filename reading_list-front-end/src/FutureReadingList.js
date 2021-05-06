7877a66472e6c074d410a115a66b25d6cebbfb48
adospinoiu <alex.dospinoiu@gmail.com>
2021-05-06 05:43:52 -0700

    Got the render function to work correctly when passing a new book

--- reading_list-front-end/src/FutureReadingList.js
+++ reading_list-front-end/src/FutureReadingList.js
@@ -5,24 +5,37 @@ import NotListedLocationIcon from '@material-ui/icons/NotListedLocation';
 
 
 function FutureReadingList({ newBook }) {
-    return (
+    const futureBook = newBook.map((data) => (
         <div className="futureReadingList">
             <NotListedLocationIcon />
 
             <div className="futureReadingList__info">
-                {newBook.map((data) => (
+                <h3>{data.title}</h3>
+                <p>{data.author}</p>
+                <p>{data.recommendedBy}</p>
+            </div>
+        </div>
+    ))
+
+
+
+    return (
+        ( futureBook )
+
+        /* Dynamic-Code */ 
+    /* {newBook.map((data) => (
                     <>
                         <h3>{data.title}</h3>
                         <p>{data.author}</p>
                         <p>{data.recommendedBy}</p>
                     </>
-                ))}
+                ))} */
 
-                {/* <h3>This is the book name</h3>
+    /* Non-Dynamic Code */ 
+    /* <h3>This is the book name</h3>
                 <p>This is the author</p>
-                <p>This is who recommended it</p> */}
-            </div>
-        </div>
+                <p>This is who recommended it</p> */
+
     )
 }
 
