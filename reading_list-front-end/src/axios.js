1d71729353958d7be39592b892991d06555edbec
adospinoiu <alex.dospinoiu@gmail.com>
2021-06-25 05:41:56 -0700

    Completed deployment to Heroku

--- reading_list-front-end/src/axios.js
+++ reading_list-front-end/src/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:9000"
+    // for DEVELOPMENTAL
+    // baseURL: "http://localhost:9000"
+
+    // for PRODUCTION
+    baseURL: "https://mern-reading-list-backend.herokuapp.com"
 });
 
 export default instance;
