0d70e1d02f25ceb2ad7a98a28e3ed1613e1a1364
adospinoiu <alex.dospinoiu@gmail.com>
2021-12-01 06:18:29 -0700

    Moved the onClick function to fetch the data to add to the notes, from the main <div> to the <icon div>

--- reading_list-front-end/src/CurrentlyReadingList.js
+++ reading_list-front-end/src/CurrentlyReadingList.js
@@ -9,35 +9,41 @@ import FactCheckIcon from '@mui/icons-material/FactCheck';
 
 function CurrentlyReadingList({ currentBook, getBookData }) {
     const readingNowBook = currentBook.map((data) => (
-        <div 
+        <div
             key={data._id}
             className="currentlyReadingList"
-            onClick={() => getBookData(data)}
+            // onClick={() => getBookData(data)}
         >
 
             <div
                 key={data._id}
                 className="currentlyReadingList__info"
             >
-                <h4>{data.title}</h4> 
+                <h4>{data.title}</h4>
                 <p>{data.author}</p>
-                
+
             </div>
 
             <div className="currentlyReadingList__iconButtons">
-                <div className="emojiObjectIcon">
+                <div className="emojiObjectIcon"
+                    onClick={() => getBookData(data)}
+                >
                     <IconButton><EmojiObjectsIcon /></IconButton>
                 </div>
 
-                <div className="factCheckIcon">
+                <div className="factCheckIcon"
+                    onClick={() =>
+                        console.log("Reading Complete. Move to Completed")
+                    }
+                >
                     <IconButton><FactCheckIcon /></IconButton>
                 </div>
-            </div>            
+            </div>
         </div>
     ))
 
     return (
-        ( readingNowBook )
+        (readingNowBook)
     )
 }
 
